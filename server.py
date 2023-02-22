94e82407c08848bb59a4446ad6581105dce99114
iustina-d <iustina2dulea@gmail.com>
2023-02-22 12:15:02 +0200

    more changes

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask
+# ,render_template
 import os
 # import data_manager
 
@@ -14,7 +15,9 @@ app = Flask(__name__)
 def main():
   return "hello ius"
   # return render_template("home.html")
-
+@app.route("/test")
+def test():
+  return "hello ius , you make a succesfully test"
 # @app.route("/search")
 # def products():
 #   all_products = data_manager.get_all_products()
@@ -29,24 +32,24 @@ def main():
 #   details = get_product["info"].split(',')
 #   return render_template("product.html",product=get_product,details=details)
 
-@app.route("/login")
-def login():
-  return render_template("login.html")
-@app.route("/registrate")
-def registrate():
-  return render_template("registrate.html")
-@app.route("/cart")
-def cart():
-  return render_template("cart.html")
-@app.route("/card")
-def card():
-  return render_template("card.html")
-@app.route("/about-us")
-def about_us():
-  return render_template("about_us.html")
-@app.route("/contact-us")
-def contact_us():
-  return render_template("contact_us.html")
+# @app.route("/login")
+# def login():
+#   return render_template("login.html")
+# @app.route("/registrate")
+# def registrate():
+#   return render_template("registrate.html")
+# @app.route("/cart")
+# def cart():
+#   return render_template("cart.html")
+# @app.route("/card")
+# def card():
+#   return render_template("card.html")
+# @app.route("/about-us")
+# def about_us():
+#   return render_template("about_us.html")
+# @app.route("/contact-us")
+# def contact_us():
+#   return render_template("contact_us.html")
 
-if __name__ == "__main__":
-  app.run(debug=False,host='0.0.0.0')
+# if __name__ == "__main__":
+#   app.run(debug=False,host='0.0.0.0')
